ab6d52d65beb78245d80558c550c7a32d46a63b3
Márcio Souza <62972594+profmsouza@users.noreply.github.com>
2024-01-12 17:48:06 -0300

    Update add.py

--- add.py
+++ add.py
@@ -1,5 +1,5 @@
 def make_q(u):
-      return 'https://quickchart.io/qr?text='+u+'&light=ffffff00&'
+      return 'https://quickchart.io/qr?text='+u+'&light=ffffff00&&dark=ffff00'
 
 def load_font(s):
       return ImageFont.truetype("f.ttf", s)
